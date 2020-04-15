6e14adc7db64901e51c753886e599b2ff32a434d
emperorakashi4 <63154092+emperorakashi4@users.noreply.github.com>
2020-04-15 08:17:16 +0530

    Update spamV2.py

--- userbot/plugins/spamV2.py
+++ userbot/plugins/spamV2.py
@@ -10,3 +10,35 @@ async def tmeme(e):
      for letter in message:
          await e.respond(letter)
      await e.delete()
+     
+     
+@borg.on(admin_cmd("spam"))
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
+        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#SPAM \n\n"
+                "Spam was executed successfully"
+                )
+                               
+@borg.on(admin_cmd("bigspam"))
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#BIGSPAM \n\n"
+                "Bigspam was executed successfully"
+                )
